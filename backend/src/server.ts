9b661d5217b51a347ce908ab451d8041a7ef3e22
cogata <carol.ogata1@gmail.com>
2024-09-16 22:27:02 -0300

    development

---
+++ backend/src/server.ts
@@ -0,0 +1,43 @@
+import fastify from "fastify";
+import { prisma } from "./lib/prisma";
+import { createTrip } from "./routes/create-trip";
+import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
+import { confirmTrip } from "./routes/confirm-trip";
+import cors from '@fastify/cors'
+import { confirmParticipants } from "./routes/confirm-participant";
+import { createActivity } from "./routes/create-activity";
+import { getActivities } from "./routes/get-activities";
+import { createLink } from "./routes/create-link";
+import { getLinks } from "./routes/get-links";
+import { getParticipants } from "./routes/get-participants";
+import { create } from "domain";
+import { createInvite } from "./routes/create-invite";
+import { updateTrip } from "./routes/update-trip";
+import { getTripDetails } from "./routes/get-trip-details";
+import { getParticipant } from "./routes/get-participant";
+
+const app = fastify()
+
+app.register(cors, {
+    origin: '*',
+})
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.register(createTrip)
+app.register(confirmTrip)
+app.register(confirmParticipants)
+app.register(createActivity)
+app.register(getActivities)
+app.register(createLink)
+app.register(getLinks)
+app.register(getParticipants)
+app.register(createInvite)
+app.register(updateTrip)
+app.register(getTripDetails)
+app.register(getParticipant)
+
+app.listen({ port: 3333 }).then(() => {
+    console.log('Server running!')
+})
---
+++ frontend/src/pages/create-trip/steps/invite-guest-step.tsx
@@ -0,0 +1,34 @@
+import { ArrowRight, UserRoundPlus } from "lucide-react";
+import { Button } from "../../../components/button";
+
+interface InviteGuestStepProps {
+    openGuestsModal: () => void,
+    openConfirmTripModal: () => void,
+    emailsToInvite: string[],
+}
+
+export function InviteGuestStep({
+    openGuestsModal,
+    emailsToInvite,
+    openConfirmTripModal
+}: InviteGuestStepProps) {
+    return (
+        <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3"> 
+            <button type="button" onClick={openGuestsModal} className="flex items-center gap-2 flex-1 text-left">
+            <UserRoundPlus className="size-5 text-zinc-400"/>
+            {
+                emailsToInvite.length > 0 ? (
+                <span className="text-lg text-zinc-100 flex-1">{emailsToInvite.length} pessoa(s) convidada(s)</span>
+                ) : (
+                <span className="text-lg text-zinc-400 flex-1">Quem estará na viagem?</span>
+                )
+            }
+            </button>
+
+            <Button onClick={openConfirmTripModal} variant="primary">
+                Confirmar viagem
+                <ArrowRight className="size-5"/>
+            </Button>
+        </div>
+    )
+}
